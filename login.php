8f5b9ce69e233929d6a9ef36816efb04becdc58c
Feitan12 <dolorricky7@gmail.com>
2026-02-05 11:32:16 +0800

    new file:   Pictures/ICON.png 	new file:   Pictures/apple-touch-icon.png 	new file:   Pictures/favicon-96x96.png 	new file:   Pictures/favicon.ico 	new file:   Pictures/favicon.svg 	new file:   Pictures/site.webmanifest 	new file:   Pictures/web-app-manifest-192x192.png 	new file:   Pictures/web-app-manifest-512x512.png 	modified:   login.php

--- login.php
+++ login.php
@@ -68,14 +68,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AMGC - Login</title>
-    <!-- Bootstrap 5 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <style>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>AMGC</title>
+<link rel="icon" type="image/png" href="Pictures/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="Pictures/favicon.svg" />
+<link rel="shortcut icon" href="Pictures/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="Pictures/apple-touch-icon.png" />
+<link rel="manifest" href="Pictures/site.webmanifest" />
+<!-- Bootstrap 5 CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+<style>
         :root {
             --primary-green: #44D34E;
             --secondary-green: #44D34E;
