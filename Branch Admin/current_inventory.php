5efc9bc5bee1689d68d5480cee9e3077685eecfc
jillalu <jillanuran4@gmail.com>
2026-02-06 15:50:58 +0800

    modified:   Branch Admin/bad_orders.php 	modified:   Branch Admin/branch_admin.php 	modified:   Branch Admin/current_inventory.php 	modified:   Branch Admin/pick_list_items.php 	modified:   Branch Admin/purchase_order.php 	modified:   Branch Admin/sales_order.php 	modified:   Branch Admin/trip_tickets.php

--- Branch Admin/current_inventory.php
+++ Branch Admin/current_inventory.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Current Inventory</title>
+    <link rel="icon" type="image/png" href="../Pictures/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="../Pictures/favicon.svg" />
+    <link rel="shortcut icon" href="../Pictures/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="../Pictures/apple-touch-icon.png" />
+    <link rel="manifest" href="../Pictures/site.webmanifest" />
     <link rel="stylesheet" href="../css/style.css">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
