c8f5e39e4f3d7ee66b228d52dc58a6525c7ea099
Feitan12 <dolorricky7@gmail.com>
2026-02-09 15:02:17 +0800

    modified:   login.php

--- login.php
+++ login.php
@@ -435,7 +435,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <!-- Mobile Logo (Visible only on mobile) -->
             <div class="mobile-logo">
                 <!-- Mobile Logo - Replace with your mobile logo image -->
-                <img src="AMGC3DLOGOICON.png" alt="AMGC Logo">
+                <img src="Pictures/AMGC3DLOGOICON.png" alt="AMGC LOGO">
             </div>
 
             <!-- Header -->
